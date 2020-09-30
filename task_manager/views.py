55339876f63fba2b21317998a4a7300bcb16f544
veetors <veetor.s@gmail.com>
2020-09-30 08:51:50 +0300

    add flash messages

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from task_manager.forms import SignupForm
+from django.contrib import messages
 
 
 def home(request):
@@ -11,9 +12,14 @@ def signup(request):
         form = SignupForm(request.POST)
         if form.is_valid():
             form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(
+                request,
+                'Account was created for {0}'.format(username),
+            )
             return redirect('home')
     else:
         form = SignupForm()
     return render(request, 'signup.html', context={
-            'form': form,
+        'form': form,
     })
